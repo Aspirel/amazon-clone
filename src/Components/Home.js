6232e1207d4b4e80fcd2e1fcf8582ab8913e4360
Aspirel <jonmultimedia1@gmail.com>
2020-09-17 22:15:45 +0100

    props added to products

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,18 +9,49 @@ function Home() {
         <img className="home_img" src="backgroundPic1.jpg" />
 
         <div className="home_row">
-          <Product />
-          <Product />
+          <Product
+            title="Buried: The thrilling new crime series introducing Detective Jack Warr
+          Kindle Edition"
+            price={1.0}
+            image="https://m.media-amazon.com/images/I/51XKMqal5nL._AC_SY240_.jpg"
+            rating={3}
+          />
+          <Product
+            title="BONTEC Triple Arm Desk Mount LCD LED Computer Monitor Bracket Stand 13”-24” Screen TV"
+            price={41.99}
+            image="https://m.media-amazon.com/images/I/61KdVGLJnKL._AC_UY218_.jpg"
+            rating={4}
+          />
         </div>
 
         <div className="home_row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title="Samsung 970 EVO 1 TB PCIe NVMe M.2 (2280) Internal Solid State Drive (SSD) (MZ-V7E1T0)"
+            price={148.12}
+            image="https://m.media-amazon.com/images/I/61q8oqAg2pL._AC_UY218_.jpg"
+            rating={5}
+          />
+          <Product
+            title="Blade Hawks RGB Gaming Headphone Stand with 3.5mm AUX and 2 USB Ports for PC, Durable Headset Stand Holder for Bose, Beats, Sony, Sennheiser, Jabra, JBL, AKG, Fancy Gaming Accessories - HS18"
+            price={22.59}
+            image="https://m.media-amazon.com/images/I/61OPbD+d4XL._AC_UY218_.jpg"
+            rating={4}
+          />
+          <Product
+            title="Corsair VOID ELITE RGB Wireless Gaming Headset (7.1 Surround Sound, Low Latency 2.4 GHz Wireless, 40ft Wireless Range, Customisable RGB Lighting, Durable Aluminium with PC, PS4 Compatibility) - Black"
+            price={99.98}
+            image="https://m.media-amazon.com/images/I/816xdLbjE4L._AC_UY218_.jpg"
+            rating={4}
+          />
         </div>
 
         <div className="home_row">
-          <Product />
+          <Product
+            title='Samsung 50" TU7100 HDR Smart 4K TV with Tizen OS'
+            price={429.0}
+            image="https://m.media-amazon.com/images/I/81pizRn9ieL._AC_UY218_.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./CSS/Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product_info">
-        <p>
-          Buried: The thrilling new crime series introducing Detective Jack Warr
-          Kindle Edition
-        </p>
+        <p>{title}</p>
         <p className="product_price">
           <small>£</small>
-          <strong>1.00</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product_rating">
-          <p>&#11088;</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>&#11088;</p>
+            ))}
         </div>
       </div>
 
-      <img src="https://m.media-amazon.com/images/I/51XKMqal5nL._AC_SY240_.jpg" />
+      <img src={image} />
 
       <button className="product_button">Buy Now</button>
     </div>
